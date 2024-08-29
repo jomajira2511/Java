97912e0c0f5fd8fb46060016c7e20d691912b4ce
Jhon <54144793+jomajira2511@users.noreply.github.com>
2024-08-29 09:10:29 -0500

    Add files via upload

---
+++ 1. Introduccion a Java/IntroduccionJava/src/Presentation.java
@@ -0,0 +1,7 @@
+public class Presentation {
+    public static void main(String[] args) {
+        System.out.println("Name : Jhon Mario Jimenez ");
+        System.out.println("Age : 24 years");
+        System.out.println("Country : Colombia ");
+    }
+}
