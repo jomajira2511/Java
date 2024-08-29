e3055e5804b41addc43b4c41edaad15c8adf285d
Jhon <54144793+jomajira2511@users.noreply.github.com>
2024-08-29 09:11:00 -0500

    Update Presentation.java

--- 1. Introduccion a Java/IntroduccionJava/src/Presentation.java
+++ 1. Introduccion a Java/IntroduccionJava/src/Presentation.java
@@ -1,6 +1,6 @@
 public class Presentation {
     public static void main(String[] args) {
-        System.out.println("Name : Jhon Mario Jimenez ");
+        System.out.println("Name : Jhon  ");
         System.out.println("Age : 24 years");
         System.out.println("Country : Colombia ");
     }
